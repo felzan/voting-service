491924f7fc091f80c105f6b528990ed7678ccb49
Luis Felipe Zanatto <luis.alves@e-unicred.com.br>
2020-06-07 20:10:08 -0300

    Initial commit

---
+++ src/test/java/com/felzan/southsystem/SouthsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felzan.southsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouthsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
