52d4a657730fb6188f5a34b5138ff271fbb2d8c9
Luís Felipe Zanatto <luisfelipe.zanatto@gmail.com>
2020-06-07 23:04:54 -0300

    Add option to start a session from a topic

--- src/main/java/com/felzan/southsystem/api/v1/TopicController.java
+++ src/main/java/com/felzan/southsystem/api/v1/TopicController.java
@@ -4,29 +4,42 @@ import com.felzan.southsystem.business.TopicService;
 import com.felzan.southsystem.dto.TopicDTO;
 import com.felzan.southsystem.dto.TopicDTORequest;
 import com.felzan.southsystem.entity.Topic;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping(value = "/topics", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TopicController {
 
-	TopicService service;
+	@Value("${defaultSessionTime:1}")
+	private Integer defaultSessionTime;
+	private final TopicService service;
 
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Topic> createTopic(@RequestBody TopicDTORequest request) {
-		if (Objects.isNull(request.getSessionEnd())) {
-			request.setSessionEnd(LocalDateTime.now());
-		}
 		return ResponseEntity.ok(service.save(request));
+	}
+
+	@PutMapping(value = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Topic> startSession(@PathVariable("id") Integer id,
+                                              @RequestParam(value = "sessionEnd", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime sessionEnd) throws Exception {
+		if (Objects.isNull(sessionEnd)) {
+			sessionEnd = LocalDateTime.now().plusMinutes(defaultSessionTime);
+		}
+		return ResponseEntity.ok(service.startSession(id, sessionEnd));
 	}
 }
--- src/main/java/com/felzan/southsystem/business/TopicService.java
+++ src/main/java/com/felzan/southsystem/business/TopicService.java
@@ -7,6 +7,9 @@ import com.felzan.southsystem.repository.TopicRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class TopicService {
@@ -16,7 +19,16 @@ public class TopicService {
 	public Topic save(TopicDTORequest request) {
 		Topic topic = new Topic();
 		topic.setDescription(request.getDescription());
-		topic.setSessionEnd(request.getSessionEnd());
 		return repository.save(topic);
+	}
+
+	public Topic startSession(Integer id, LocalDateTime sessionEnd) throws Exception {
+		Optional<Topic> optionalTopic = repository.findById(id);
+		if (optionalTopic.isPresent()) {
+			Topic topic = optionalTopic.get();
+			topic.setSessionEnd(sessionEnd);
+			return repository.save(topic);
+		}
+		throw new Exception("Topic not found");
 	}
 }
